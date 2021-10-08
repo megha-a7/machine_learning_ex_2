6e264ee2d777ed6ece140d24849ddf8ddee47757
megha-a7 <82528894+megha-a7@users.noreply.github.com>
2021-10-08 17:20:25 +0530

    Add files via upload

---
+++ ex2-octave/plotData.m
@@ -0,0 +1,30 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+
+
+epsilon_init = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
